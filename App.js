e97252574412b9bc85d537509ed0c5fb1d9cfd30
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-02-03 16:11:42 -0500

    [+] Component > App: Crea 4 Layouts verticales usando flexbox

--- App.js
+++ App.js
@@ -4,13 +4,39 @@ import { StyleSheet, View } from 'react-native';
 const App = () => {
     return (
         <>
-            
+            <View style={ styles .container }>
+                <View style={ styles .box1 }></View>
+                <View style={ styles .box2 }></View>
+                <View style={ styles .box3 }></View>
+                <View style={ styles .box4 }></View>
+            </View>
         </>
     );
 };
 
 const styles = StyleSheet.create({
-  
+    /** Flexbox para React Native usa flexbox por defecto y su direccion por defecto es vertical, es decir: flexDirection: column */
+    container: {
+        backgroundColor: 'cornflowerblue',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    box1: {
+        backgroundColor: 'navy',
+        padding: 20
+    },
+    box2: {
+        backgroundColor: 'yellow',
+        padding: 20
+    },
+    box3: {
+        backgroundColor: 'green',
+        padding: 20
+    },
+    box4: {
+        backgroundColor: 'teal',
+        padding: 20
+    },
 });
 
 export default App;
