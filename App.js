7298c34bbdb16649627e78e0ae7f1c28ea609ae6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-02-03 19:21:57 -0500

    [+] Component > App: Crea Layout para desplegar banner [+] Assets > Images

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 
 const App = () => {
     return (
         <>
-            <View style={ styles .container }>
-                <View style={ styles .box1 }></View>
-                <View style={ styles .box2 }></View>
-                <View style={ styles .box3 }></View>
-                <View style={ styles .box4 }></View>
+            <View style={{ flexDirection: 'row' }}>
+                <Image 
+                    style={ styles .banner }
+                    source={ require( './src/assets/img/bg.jpg' ) }
+                />
             </View>
         </>
     );
@@ -16,29 +16,10 @@ const App = () => {
 
 const styles = StyleSheet.create({
     /** Flexbox para React Native usa flexbox por defecto y su direccion por defecto es vertical, es decir: flexDirection: column */
-    container: {
-        backgroundColor: 'cornflowerblue',
-        flex: 1,                         //  Si el padre no se extiende los hijos tampoco
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        alignItems: "center"            //  Por defecto trae 'stretch'
-    },
-    box1: {
-        backgroundColor: 'navy',
-        padding: 20
-    },
-    box2: {
-        backgroundColor: 'yellow',
-        padding: 20
-    },
-    box3: {
-        backgroundColor: 'green',
-        padding: 20
-    },
-    box4: {
-        backgroundColor: 'teal',
-        padding: 20
-    },
+    banner: {
+        flex: 1,
+        height: 250
+    }
 });
 
 export default App;
