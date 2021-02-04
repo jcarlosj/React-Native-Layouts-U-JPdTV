a3f91187ec9310076b4f5669e2e9d0bdf8dd73c0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-02-04 15:16:10 -0500

    [+] Component > App: Crea ScrollView Horizontal de Imagenes

--- App.js
+++ App.js
@@ -1,15 +1,48 @@
 import React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { Image, StyleSheet, Text, View, ScrollView } from 'react-native';
+
+const logo = {
+    uri: 'https://reactnative.dev/img/tiny_logo.png',
+    width: 64,
+    height: 64
+};
 
 const App = () => {
     return (
         <>
-            <View style={{ flexDirection: 'row' }}>
+            <View style={{ flexDirection: 'row' }}>        
                 <Image 
                     style={ styles .banner }
                     source={ require( './src/assets/img/bg.jpg' ) }
                 />
             </View>
+                
+            <View style={ styles .container }>
+                <Text style={ styles .title }>Qué hacer en París</Text>
+
+                <ScrollView horizontal={ true }>
+                    {/* Se podría usar un View usando Flexbox pero ScrollView ya soluciona el problema */}
+                    <Image 
+                        style={ styles .city }
+                        source={ require( './src/assets/img/actividad1.jpg' ) }
+                    />
+                    <Image 
+                        style={ styles .city }
+                        source={ require( './src/assets/img/actividad2.jpg' ) }
+                    />
+                    <Image 
+                        style={ styles .city }
+                        source={ require( './src/assets/img/actividad3.jpg' ) }
+                    />
+                    <Image 
+                        style={ styles .city }
+                        source={ require( './src/assets/img/actividad4.jpg' ) }
+                    />
+                    
+                </ScrollView>
+
+            </View>
+            
         </>
     );
 };
@@ -19,6 +52,19 @@ const styles = StyleSheet.create({
     banner: {
         flex: 1,
         height: 250
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginVertical: 10
+    },
+    container: {
+        marginHorizontal: 10
+    },
+    city: {
+        height: 300,
+        marginRight: 10,
+        width: 250, 
     }
 });
 
