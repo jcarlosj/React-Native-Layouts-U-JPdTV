6d14e6f173adda3ae7a5b32ca898ecb5fa084fc6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-02-04 16:04:18 -0500

    [+] Component > App: Crea grilla de imagenes 4 x 4 con descripcion & Copyright

--- App.js
+++ App.js
@@ -67,8 +67,46 @@ const App = () => {
 
                     </View>
 
+                    <Text style={ styles .title }>Hospedajes en los LA</Text>
+                    <View
+                        style={ styles .list }
+                    >
+
+                        <View style={ styles .listItem }>     
+                            <Image 
+                                style={ styles .best }
+                                source={ require( './src/assets/img/hospedaje1.jpg' ) }
+                            />
+                            <Text style={ styles .description }>Salón de juegos</Text>
+                        </View>
+                        <View style={ styles .listItem }>     
+                            <Image 
+                                style={ styles .best }
+                                source={ require( './src/assets/img/hospedaje2.jpg' ) }
+                            />
+                            <Text style={ styles .description }>Salón de reuniones</Text>
+                        </View>
+                        <View style={ styles .listItem }>     
+                            <Image 
+                                style={ styles .best }
+                                source={ require( './src/assets/img/hospedaje3.jpg' ) }
+                            />
+                            <Text style={ styles .description }>Lo mejor para la familia</Text>
+                        </View>
+                        <View style={ styles .listItem }>     
+                            <Image 
+                                style={ styles .best }
+                                source={ require( './src/assets/img/hospedaje4.jpg' ) }
+                            />
+                            <Text style={ styles .description }>Grandes espacios</Text>
+                        </View>
+
+                    </View>
+
                 </View>
                 
+                <Text style={ styles .copyright }>Copyright @ 2021</Text>
+
             </ScrollView>
         </>
     );
@@ -97,6 +135,21 @@ const styles = StyleSheet.create({
         height: 180,
         marginVertical: 5,
         width: '100%'
+    },
+    list: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between'     //  gap no funciona en Flexbox
+    },
+    listItem: {
+        flexBasis: '49%'       /** Igual que un width cuando se usa FlexBox */
+    },
+    description: {
+        fontSize: 12
+    },
+    copyright: {
+        textAlign: 'center',
+        marginVertical: 15
     }
 });
 
